23a50ed1444c4fe3f36c93771e124f0fb94f10a3
H4RR1S-D3V <harrisdm.dev@gmail.com>
2025-05-09 20:48:21 -0300

    Clase de Conexion

---
+++ TP6_Grupo19/Clases/Conexion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TP6_Grupo19.Clases
+{
+    public class Conexion
+    {
+        private string _cadenaConexion = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+        private readonly SqlConection _sqlConnection;
+        private string _consultaSQL;
+
+        public Conexion()
+        {
+            _sqlConnection = new SqlConection(_cadenaConexion);
+
+        }
+
+        public DataTable TraerTabla(string consultaSql, string nombreTabla)
+        {
+            _consultaSQL = "SELECT IdProducto, NombreProducto, CantidadPorUnidad, PrecioUnidad FROM Productos"
+
+            _sqlConnection.Open();
+
+            SqlDataAdapter adapter = new SqlDataAdapter(consultaSQL, _sqlConnection);
+            DataSet dataSet = new DataSet();
+            adapter.Fill(dataSet, nombreTabla);
+
+            _sqlConnection.Close();
+
+            return dataSet.Tables[nombreTabla];
+        }
+    }
+}
+    
