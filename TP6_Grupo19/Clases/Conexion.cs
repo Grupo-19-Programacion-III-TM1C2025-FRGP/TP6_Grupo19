9534730fd780d7cd4f40f7417151510a5edb0f93
H4RR1S-D3V <harrisdm.dev@gmail.com>
2025-05-10 01:56:04 -0300

    Arreglo-Clase-Conexion

--- TP6_Grupo19/Clases/Conexion.cs
+++ TP6_Grupo19/Clases/Conexion.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
-using System.Data.SqlClient;
-using System.Data;
+using System.Web.UI;
 
 namespace TP6_Grupo19.Clases
 {
     public class Conexion
     {
         private string _cadenaConexion = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=Neptuno;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
-        private readonly SqlConection _sqlConnection;
+        private readonly SqlConnection _sqlConnection;
         private string _consultaSQL;
 
         public Conexion()
         {
-            _sqlConnection = new SqlConection(_cadenaConexion);
+            _sqlConnection = new SqlConnection(_cadenaConexion);
 
         }
 
         public DataTable TraerTabla(string consultaSql, string nombreTabla)
         {
-            _consultaSQL = "SELECT IdProducto, NombreProducto, CantidadPorUnidad, PrecioUnidad FROM Productos"
+            _consultaSQL = "SELECT IdProducto, NombreProducto, CantidadPorUnidad, PrecioUnidad FROM Productos";
 
             _sqlConnection.Open();
 
-            SqlDataAdapter adapter = new SqlDataAdapter(consultaSQL, _sqlConnection);
+            SqlDataAdapter adapter = new SqlDataAdapter(_consultaSQL, _sqlConnection);
             DataSet dataSet = new DataSet();
             adapter.Fill(dataSet, nombreTabla);
 
